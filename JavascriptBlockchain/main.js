c85f5663969b61922d035323c6b553984b2f7803
park jun hyuk <junhyeg08@gmail.com>
2021-12-18 23:49:10 +0900

    Feat: Blockchain class 추가

--- JavascriptBlockchain/main.js
+++ JavascriptBlockchain/main.js
@@ -14,6 +14,23 @@ class Block {
   }
 }
 
-// class Blockchain {
-//   constructor
-// }
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2017", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
