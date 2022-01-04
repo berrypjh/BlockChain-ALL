f70a9e55037f633754c58a16963cc7caea2a976a
berrypjh <junhyeg08@gmail.com>
2022-01-05 00:49:36 +0900

    Feat: POW - nonce, Difficulty 추가 / adjustDifficulty 함수 추가

--- Cryptocurrency/block.js
+++ Cryptocurrency/block.js
@@ -1,12 +1,14 @@
-const { GENESIS_DATA } = require("./config");
+const { GENESIS_DATA, MINE_RATE } = require("./config");
 const cryptoHash = require("./crypto-hash");
 
 class Block {
-  constructor({ timestamp, lastHash, hash, data }) {
+  constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty;
   }
 
   static genesis() {
@@ -14,15 +16,26 @@ class Block {
   }
 
   static minedBlock({ lastBlock, data }) {
-    const timestamp = Date.now();
+    let hash, timestamp;
     const lastHash = lastBlock.hash;
+    const { difficulty } = lastBlock;
+    let nonce = 0;
 
-    return new this({
-      timestamp,
-      lastHash,
-      data,
-      hash: cryptoHash(timestamp, lastHash, data)
-    });
+    do {
+      nonce++;
+      timestamp = Date.now();
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+
+    return new this({ timestamp, lastHash, data, difficulty, nonce, hash });
+  }
+
+  static adjustDifficulty({ originalBlock, timestamp }) {
+    const { difficulty } = originalBlock;
+    
+    if ((timestamp - originalBlock.timestamp) > MINE_RATE) return difficulty - 1;
+
+    return difficulty + 1;
   }
 }
 
