b23e132a878e58f9563d667471a3e40d24ca752c
Junhyuk Park <junhyeg08@gmail.com>
2022-04-07 22:24:29 +0900

    Feat: Next.js layout

---
+++ NextjsEth/pages/index.js
@@ -0,0 +1,20 @@
+import { Breadcrumbs, Hero } from "@components/common";
+import { CourseList } from "@components/course";
+import { BaseLayout } from "@components/layout";
+import { OrderCard } from "@components/order";
+import { EthRates, WalletBar } from "@components/web3";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Breadcrumbs />
+      <WalletBar />
+      <EthRates />
+      <OrderCard />
+      <CourseList />
+    </>
+  );
+}
+
+Home.Layout = BaseLayout;
