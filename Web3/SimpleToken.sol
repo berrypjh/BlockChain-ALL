11f6035bfdd86c3cf8ec5d90f653b8de161aef86
Junhyuk Park <junhyeg08@gmail.com>
2022-10-23 15:10:08 +0900

    Refactor: solidity 정리

--- Web3/SimpleToken.sol
+++ Web3/SimpleToken.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.14;
 
 interface ERC20Interface {
-    function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
     function approve(address spender, uint256 amount) external returns (bool);
@@ -15,83 +14,67 @@ interface ERC20Interface {
 }
 
 contract SimpleToken is ERC20Interface {
-    mapping (address => uint256) private _balances;
-    mapping (address => mapping (address => uint256)) public _allowances;
+    mapping (address => uint256) private balances;
+    mapping (address => mapping (address => uint256)) private allowances;
 
-    uint256 public _totalSupply;
-    string public _name;
-    string public _symbol;
-    uint8 public _decimals;
+    uint256 public totalSupply;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
     uint private E18 = 1000000000000000000;
 
-    constructor(string memory getName, string memory getSymbol) {
-        _name = getName;
-        _symbol = getSymbol;
-        _decimals = 18;
-        _totalSupply = 100000000 * E18;
-        _balances[msg.sender] = _totalSupply; // 추가
+    constructor(string memory _getName, string memory _getSymbol) {
+        name = _getName;
+        symbol = _getSymbol;
+        decimals = 18;
+        totalSupply = 100000000 * E18;
+        balances[msg.sender] = totalSupply; 
     }
 
-    function name() public view returns (string memory) {
-        return _name;
+    function balanceOf(address _account) external view virtual override returns (uint256) {
+        return balances[_account];
     }
 
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-
-    function decimals() public view returns (uint8) {
-        return _decimals;
-    }
-
-    function totalSupply() external view virtual override returns (uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) external view virtual override returns (uint256) {
-        return _balances[account];
-    }
-
-    function transfer(address recipient, uint amount) public virtual override returns (bool) {
-        _transfer(msg.sender, recipient, amount);
-        emit Transfer(msg.sender, recipient, amount);
+    function transfer(address _recipient, uint _amount) public virtual override returns (bool) {
+        transferToken(msg.sender, _recipient, _amount);
+        emit Transfer(msg.sender, _recipient, _amount);
         return true;
     }
 
-    function allowance(address owner, address spender) external view override returns (uint256) {
-        return _allowances[owner][spender];
+    function allowance(address _owner, address _spender) external view override returns (uint256) {
+        return allowances[_owner][_spender];
     }
 
-    function approve(address spender, uint amount) external virtual override returns (bool) {
-        uint256 currentAllowance = _allowances[msg.sender][spender];  
-        require(_balances[msg.sender] >= amount,"ERC20: The amount to be transferred exceeds the amount of tokens held by the owner.");
-        _approve(msg.sender, spender, currentAllowance, amount);
+    function approve(address _spender, uint _amount) external virtual override returns (bool) {
+        uint256 currentAllowance = allowances[msg.sender][_spender];  
+        require(balances[msg.sender] >= _amount,"ERC20: The amount to be transferred exceeds the amount of tokens held by the owner.");
+        approveToken(msg.sender, _spender, currentAllowance, _amount);
         return true;
     }
 
-    function transferFrom(address sender, address recipient, uint256 amount) external virtual override returns (bool) {
-        _transfer(sender, recipient, amount);
-        emit Transfer(msg.sender, sender, recipient, amount);
-        uint256 currentAllowance = _allowances[sender][msg.sender];
-        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
-        _approve(sender, msg.sender, currentAllowance, currentAllowance - amount);
+    function transferFrom(address _sender, address _recipient, uint256 _amount) external virtual override returns (bool) {
+        transferToken(_sender, _recipient, _amount);
+        emit Transfer(msg.sender, _sender, _recipient, _amount);
+        uint256 currentAllowance = allowances[_sender][msg.sender];
+        require(currentAllowance >= _amount, "ERC20: transfer amount exceeds allowance");
+        approveToken(_sender, msg.sender, currentAllowance, currentAllowance - _amount);
         return true;
     }
 
-    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-        uint256 senderBalance = _balances[sender];
-        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
-        _balances[sender] = senderBalance - amount;
-        _balances[recipient] += amount;
+    function transferToken(address _sender, address _recipient, uint256 _amount) internal virtual {
+        require(_sender != address(0), "ERC20: transfer from the zero address");
+        require(_recipient != address(0), "ERC20: transfer to the zero address");
+        uint256 senderBalance = balances[_sender];
+        require(senderBalance >= _amount, "ERC20: transfer amount exceeds balance");
+        balances[_sender] = senderBalance - _amount;
+        balances[_recipient] += _amount;
     }
 
-    function _approve(address owner, address spender, uint256 currentAmount, uint256 amount) internal virtual {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
-        require(currentAmount == _allowances[owner][spender], "ERC20: invalid currentAmount");
-        _allowances[owner][spender] = amount;  
-        emit Approval(owner, spender, currentAmount, amount);
+    function approveToken(address _owner, address _spender, uint256 _currentAmount, uint256 _amount) internal virtual {
+        require(_owner != address(0), "ERC20: approve from the zero address");
+        require(_spender != address(0), "ERC20: approve to the zero address");
+        require(_currentAmount == allowances[_owner][_spender], "ERC20: invalid currentAmount");
+        allowances[_owner][_spender] = _amount;  
+        emit Approval(_owner, _spender, _currentAmount, _amount);
     }
 }
